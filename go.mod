809506e292ee7139646a3a389cde5207c51cf5a9
Matthieu Miser <matthieumiser94@gmail.com>
2023-09-21 13:44:38 +0300

    added websockets

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go-htmx-chat
 
 go 1.21.0
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/google/uuid v1.3.1
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.5.0
+	golang.org/x/net v0.15.0
+)
--- main.go
+++ main.go
@@ -2,16 +2,73 @@ package main
 
 import (
 	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"sync"
+
+	"golang.org/x/net/websocket"
 
 	"github.com/gorilla/mux"
 )
 
+type Server struct {
+	mu    sync.Mutex
+	conns map[*websocket.Conn]bool
+}
+
 func main() {
 	r := mux.NewRouter()
+	server := newServer()
+
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	r.HandleFunc("/", homeRoute)
+
+	r.Handle("/ws", websocket.Handler(server.handleWs))
+
 	http.ListenAndServe(":8080", r)
+}
+
+func newServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWs(ws *websocket.Conn) {
+	log.Println("Established connection with ", ws.RemoteAddr())
+	s.mu.Lock()
+	s.conns[ws] = true
+	s.mu.Unlock()
+
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Println("Error reading buffer:", err)
+			continue
+		}
+		msg := buf[:n]
+
+		s.broadcast(msg)
+	}
+}
+
+func (s *Server) broadcast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				log.Println("write error ", err)
+			}
+		}(ws)
+	}
 }
 
 func homeRoute(w http.ResponseWriter, r *http.Request) {
